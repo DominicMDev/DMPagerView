867232d8cf124233a5b1795d904da75ee770d6fb
Dominic Miller <Dominic@app-order.com>
2018-09-06 09:33:19 -0700

    Initial Commit

---
+++ DMPagerView/DMPagerView.h
@@ -0,0 +1,19 @@
+//
+//  DMPagerView.h
+//  DMPagerView
+//
+//  Created by Dominic Miller on 9/6/18.
+//  Copyright © 2018 Dominic Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DMPagerView.
+FOUNDATION_EXPORT double DMPagerViewVersionNumber;
+
+//! Project version string for DMPagerView.
+FOUNDATION_EXPORT const unsigned char DMPagerViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DMPagerView/PublicHeader.h>
+
+
